8869fa5e21b84a20ea3c9dc37c23c422674afd9c
tarekOumansour <tarekoumansour11@gmail.com>
2024-04-10 15:51:47 +0100

    updated args

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,19 @@
 const puppeteer = require("puppeteer") ; 
+require("dotenv").config() ; 
 const scrapeLogic = async (res) => { 
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            args : [
+                "--disable-setuid-sandbox" , 
+                "--no-sandbox" , 
+                "--single-process",
+                "--no-zygote" ,
+            ],
+            executablePath : 
+              process.env.NODE_ENV === "production" 
+              ? process.env.PUPPETEER_EXECUTABLE_PATH
+              : puppeteer.executablePath , 
+        });
         const page = await browser.newPage();
         await page.goto('https://www.npmjs.com/package/puppeteer');
         //await page.screenshot({ path: 'example.png' });
